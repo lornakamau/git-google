d9a238bca53be0b41c58068da4920cb7e9aff503
Lorna Kamau <kamaulorna@gmail.com>
2020-05-15 11:04:01 +0300

    add routing to my-profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component'; 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
+  { path: 'my-profile', component: MyProfileComponent},
   { path: 'search', component: SearchComponent},
   { path: '**', component: PageNotFoundComponent},
 ];
